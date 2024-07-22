bd4663b37f9a500875f3c6535e7ecb51f6de0328
hainweb <hainofficialweb@gmail.com>
2024-07-22 19:16:16 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https://social-chikku-all.onrender.com",
 		methods: ["GET", "POST"],
 	},
 });
